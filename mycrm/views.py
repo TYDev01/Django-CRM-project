8d80953515378ae187afd514783a8c823b9b1397
Anthony <anthonychinaemeru84@yahoo.com>
2024-04-15 00:56:33 +0100

    login authentication added

--- mycrm/views.py
+++ mycrm/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username'],
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login succesfully')
+            return redirect('home')
+        else:
+            messages.success(request, 'Account not found')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+    return render(request, 'home.html')
